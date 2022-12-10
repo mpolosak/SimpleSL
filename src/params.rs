5f5b9d27b5c5bc392ba6f9fe0d15075a8c269df4
mpolosak <42770518+mpolosak@users.noreply.github.com>
2022-12-10 18:32:22 +0100

    Add get_result_var function

--- src/params.rs
+++ src/params.rs
@@ -15,23 +15,11 @@ pub trait Parse {
 
 impl Parse for ParamVec {
     fn parse(mut text: String) -> Result<(Option<String>, ParamVec), String> {
-        let result_var;
-        if let Some((before, after)) = text.split_once("=") {
-            if after.contains("="){
-                return Err(String::from("Too many = in one line"))
-            }
-            let before_s = before.trim().to_string();
-            if before_s.contains(" ") || before_s.is_empty() {
-                return Err(String::from("Before = should be exactly one variable"))
-            }
-            if !is_correct_variable_name(&before_s){
-                return Err(format!("{} isn't correct variable name", before_s));
-            }
-            result_var = Some(before_s);
-            text = String::from(after);
-        } else {
-            result_var = None
-        }
+        let result_var = match get_result_var(&mut text) {
+            Ok(value) => value,
+            Err(e) => return Err(e)
+        };
+
         let mut vec = ParamVec::new();
         while text.len()>0 {
             text = text.trim().to_string();
@@ -68,5 +56,40 @@ impl Parse for ParamVec {
             vec.push(param);
         }
         Ok((result_var, vec))
+    }
+}
+
+fn get_result_var(text: &mut String) -> Result<Option<String>, String>{
+    if let Some((before, after)) = text.split_once("=") {
+        if after.contains("="){
+            return Err(String::from("Too many = in one line"))
+        }
+        let before_s = before.trim().to_string();
+        if before_s.contains(" ") || before_s.is_empty() {
+            return Err(String::from("Before = should be exactly one variable"))
+        }
+        if !is_correct_variable_name(&before_s){
+            return Err(format!("{} isn't correct variable name", before_s));
+        }
+        *text = String::from(after);
+        Ok(Some(before_s))
+    } else { Ok(None) }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn check_get_result_var() {
+        use crate::params::get_result_var;
+        let mut text = String::from(" x = 14");
+        assert_eq!(get_result_var(&mut text), Ok(Some(String::from("x"))));
+        text = String::from(" x = 5 = 6");
+        assert_eq!(get_result_var(&mut text), Err(String::from("Too many = in one line")));
+        text = String::from(" x y = 6");
+        assert_eq!(get_result_var(&mut text),
+            Err(String::from("Before = should be exactly one variable")));
+        text = String::from(" 7 = 6");
+        assert_eq!(get_result_var(&mut text),
+            Err(String::from("7 isn't correct variable name")));
     }
 }
