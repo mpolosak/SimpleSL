001d9e53484e699cea5772d818fa0f41f5451439
mpolosak <42770518+mpolosak@users.noreply.github.com>
2022-12-24 12:32:37 +0100

    Make some functions requir exact number of params

--- src/stdfunctions.rs
+++ src/stdfunctions.rs
@@ -2,27 +2,27 @@ use crate::variable::*;
 use crate::*;
 
 pub fn add_std_functions(variables: &mut VariableMap){
-    add_function!("add", variables, params, (var1: Float, var2: Float,) {
+    add_function!("add", variables, params, only (var1: Float, var2: Float,) {
         Ok(Variable::Float(var1+var2))
     });
-    add_function!("subtract", variables, params, (var1: Float, var2: Float,){
+    add_function!("subtract", variables, params, only (var1: Float, var2: Float,){
         Ok(Variable::Float(var1-var2))
     });
-    add_function!("multiply", variables, params, (var1: Float, var2: Float,){
+    add_function!("multiply", variables, params, only (var1: Float, var2: Float,){
         Ok(Variable::Float(var1*var2))
     });
-    add_function!("divide", variables, params, (var1: Float, var2: Float,){
+    add_function!("divide", variables, params, only (var1: Float, var2: Float,){
         Ok(Variable::Float(var1/var2))
     });
-    add_function!("modulo", variables, params, (var1: Float, var2: Float,){
+    add_function!("modulo", variables, params, only (var1: Float, var2: Float,){
         let divided = var1/var2;
         let result = var1 - var2*divided.floor();
         Ok(Variable::Float(result))
     });
-    add_function!("or", variables, params, (var1: Float, var2: Float,){
+    add_function!("or", variables, params, only (var1: Float, var2: Float,){
         Ok(Variable::Float(var1.abs()+var2.abs()))
     });
-    add_function!("not", variables, params, (var: Float,){
+    add_function!("not", variables, params, only (var: Float,){
         Ok(Variable::Float(if var==0.0{1.0}else{0.0}))
     });
     add_function!("if", variables, params, (condition: Float, function: Function,){
