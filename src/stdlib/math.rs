8868b96c75c9c94f4f40b34f8c6e191a3d03ee2f
Michał Połosak <mpolosak@op.pl>
2023-06-18 22:00:30 +0200

    Math functions for int and float, conversion

--- src/stdlib/math.rs
+++ src/stdlib/math.rs
@@ -5,42 +5,71 @@ use simplesl_macros::export_math_function;
 
 pub fn add_math_functions(variables: &mut VariableMap) {
     #[export_math_function]
-    fn add(a: f64, b: f64) -> f64 {
+    fn add(a: i64, b: i64) -> i64 {
         a + b
     }
 
     #[export_math_function]
-    fn subtract(a: f64, b: f64) -> f64 {
+    fn subtract(a: i64, b: i64) -> i64 {
         a - b
     }
 
     #[export_math_function]
-    fn multiply(a: f64, b: f64) -> f64 {
+    fn multiply(a: i64, b: i64) -> i64 {
         a * b
     }
 
     #[export_math_function]
-    fn divide(a: f64, b: f64) -> f64 {
+    fn divide(a: i64, b: i64) -> i64 {
         a / b
     }
 
     #[export_math_function]
-    fn modulo(a: f64, b: f64) -> f64 {
-        let divided = a / b;
-        a - b * divided.floor()
+    fn modulo(a: i64, b: i64) -> i64 {
+        a % b
     }
 
     #[export_math_function]
-    fn or(a: f64, b: f64) -> f64 {
+    fn or(a: i64, b: i64) -> i64 {
         a.abs() + b.abs()
     }
 
     #[export_math_function]
-    fn not(a: f64) -> f64 {
-        if a == 0.0 {
-            1.0
+    fn not(a: i64) -> i64 {
+        if a == 0 {
+            1
         } else {
-            0.0
+            0
         }
+    }
+
+    #[export_math_function]
+    fn addf(a: f64, b: f64) -> f64 {
+        a + b
+    }
+
+    #[export_math_function]
+    fn subtractf(a: f64, b: f64) -> f64 {
+        a - b
+    }
+
+    #[export_math_function]
+    fn multiplyf(a: f64, b: f64) -> f64 {
+        a * b
+    }
+
+    #[export_math_function]
+    fn dividef(a: f64, b: f64) -> f64 {
+        a / b
+    }
+
+    #[export_math_function]
+    fn int_to_float(value: i64) -> f64 {
+        value as f64
+    }
+
+    #[export_math_function]
+    fn float_to_int(value: f64) -> i64 {
+        value as i64
     }
 }
