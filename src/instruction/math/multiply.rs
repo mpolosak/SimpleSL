1dae82bd6e4cd9fa73d27ce9c79666288b693b73
mpolosak <42770518+mpolosak@users.noreply.github.com>
2023-08-03 17:18:51 +0200

    * operator for array and int or float

--- src/instruction/math/multiply.rs
+++ src/instruction/math/multiply.rs
@@ -30,22 +30,45 @@ impl CreateInstruction for Multiply {
             (Type::Int, Type::Int) | (Type::Float, Type::Float) => {
                 Ok(Self::create_from_instructions(lhs, rhs))
             }
+            (Type::Array(var_type), Type::Int) | (Type::Int, Type::Array(var_type))
+                if var_type == Type::Int.into() =>
+            {
+                Ok(Self::create_from_instructions(lhs, rhs))
+            }
+            (Type::Array(var_type), Type::Float) | (Type::Float, Type::Array(var_type))
+                if var_type == Type::Float.into() =>
+            {
+                Ok(Self::create_from_instructions(lhs, rhs))
+            }
+            (Type::EmptyArray, Type::Int | Type::Float)
+            | (Type::Int | Type::Float, Type::EmptyArray) => {
+                Ok(Self::create_from_instructions(lhs, rhs))
+            }
             _ => Err(Error::OperandsMustBeBothIntOrBothFloat("*")),
         }
     }
 }
 
 impl Multiply {
+    fn multiply(lhs: Variable, rhs: Variable) -> Variable {
+        match (lhs, rhs) {
+            (Variable::Int(lhs), Variable::Int(rhs)) => (lhs * rhs).into(),
+            (Variable::Float(lhs), Variable::Float(rhs)) => (lhs * rhs).into(),
+            (array @ Variable::Array(_, Type::EmptyArray), _)
+            | (_, array @ Variable::Array(_, Type::EmptyArray)) => array,
+            (value, Variable::Array(array, _)) | (Variable::Array(array, _), value) => array
+                .iter()
+                .cloned()
+                .map(|element| Self::multiply(element, value.clone()))
+                .collect(),
+            (lhs, rhs) => panic!("Tried to multiply {lhs} and {rhs}"),
+        }
+    }
     fn create_from_instructions(lhs: Instruction, rhs: Instruction) -> Instruction {
         match (lhs, rhs) {
-            (
-                Instruction::Variable(Variable::Int(value1)),
-                Instruction::Variable(Variable::Int(value2)),
-            ) => Instruction::Variable((value1 * value2).into()),
-            (
-                Instruction::Variable(Variable::Float(value1)),
-                Instruction::Variable(Variable::Float(value2)),
-            ) => Instruction::Variable((value1 * value2).into()),
+            (Instruction::Variable(lhs), Instruction::Variable(rhs)) => {
+                Self::multiply(lhs, rhs).into()
+            }
             (lhs, rhs) => Self { lhs, rhs }.into(),
         }
     }
@@ -55,11 +78,7 @@ impl Exec for Multiply {
     fn exec(&self, interpreter: &mut Interpreter) -> Result<Variable> {
         let lhs = self.lhs.exec(interpreter)?;
         let rhs = self.rhs.exec(interpreter)?;
-        match (lhs, rhs) {
-            (Variable::Int(value1), Variable::Int(value2)) => Ok((value1 * value2).into()),
-            (Variable::Float(value1), Variable::Float(value2)) => Ok((value1 * value2).into()),
-            _ => panic!(),
-        }
+        Ok(Self::multiply(lhs, rhs))
     }
 }
 
