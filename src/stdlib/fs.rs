7024cdd7f190e4918ebd52ac72c0e3e58d98e273
mpolosak <42770518+mpolosak@users.noreply.github.com>
2023-08-10 16:16:28 +0200

    Add fs functions

--- src/stdlib/fs.rs
+++ src/stdlib/fs.rs
@@ -11,8 +11,45 @@ pub fn add_functions(interpreter: &mut Interpreter) {
     fn file_read_to_string(path: &str) -> io::Result<String> {
         fs::read_to_string(path)
     }
+
     #[export_function]
     fn write_to_file(path: &str, contents: &str) -> io::Result<()> {
         fs::write(path, contents)
+    }
+
+    #[export_function]
+    fn copy_file(from: &str, to: &str) -> io::Result<()> {
+        fs::copy(from, to)?;
+        Ok(())
+    }
+
+    #[export_function]
+    fn remove_file(path: &str) -> io::Result<()> {
+        fs::remove_file(path)
+    }
+
+    #[export_function]
+    fn remove_dir(path: &str) -> io::Result<()> {
+        fs::remove_dir(path)
+    }
+
+    #[export_function]
+    fn remove_dir_all(path: &str) -> io::Result<()> {
+        fs::remove_dir_all(path)
+    }
+
+    #[export_function]
+    fn create_dir(path: &str) -> io::Result<()> {
+        fs::create_dir(path)
+    }
+
+    #[export_function]
+    fn create_dir_all(path: &str) -> io::Result<()> {
+        fs::create_dir_all(path)
+    }
+
+    #[export_function]
+    fn rename(from: &str, to: &str) -> io::Result<()> {
+        fs::rename(from, to)
     }
 }
