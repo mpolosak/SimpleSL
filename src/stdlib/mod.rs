3ea4db9353c8874e773956dbb1a6368a97327087
mpolosak <42770518+mpolosak@users.noreply.github.com>
2023-08-11 19:31:21 +0200

    Remove int, float, string, array functions because they i trivial to implement in simplesl code using match expression

--- src/stdlib/mod.rs
+++ src/stdlib/mod.rs
@@ -3,7 +3,6 @@ mod fs;
 mod iofunctions;
 mod math;
 mod string;
-mod types;
 use crate::{
     function::{NativeFunction, Param, Params},
     interpreter::Interpreter,
@@ -14,7 +13,6 @@ use simplesl_macros::export_function;
 pub fn add_std_lib(interpreter: &mut Interpreter) {
     array::add_functions(interpreter);
     iofunctions::add_functions(interpreter);
-    types::add_functions(interpreter);
     math::add_functions(interpreter);
     string::add_functions(interpreter);
     fs::add_functions(interpreter);
