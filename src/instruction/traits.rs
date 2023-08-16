33c1358d2106cf4ac48391843fdbe03dc6535def
mpolosak <42770518+mpolosak@users.noreply.github.com>
2023-08-16 13:42:15 +0200

    Add trait CreateBinOp

--- src/instruction/traits.rs
+++ src/instruction/traits.rs
@@ -1,8 +1,10 @@
 mod bin_op;
 mod can_be_used;
+mod create_bin_op;
 mod create_from_instructions;
 pub use self::{
-    bin_op::BinOp, can_be_used::CanBeUsed, create_from_instructions::CreateFromInstructions,
+    bin_op::BinOp, can_be_used::CanBeUsed, create_bin_op::CreateBinOp,
+    create_from_instructions::CreateFromInstructions,
 };
 use super::{local_variable::LocalVariables, Instruction};
 use crate::{interpreter::Interpreter, parse::Rule, variable::Variable, Result};
