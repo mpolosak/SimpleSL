0d29187227443cd8dd2a60a30281765fbc6b93ba
Michał Połosak <mpolosak@op.pl>
2024-01-24 19:08:58 +0100

    comp operators use type matches in CanBeUsed

--- src/instruction/comp/mod.rs
+++ src/instruction/comp/mod.rs
@@ -4,24 +4,13 @@ mod greater;
 mod greater_or_equal;
 mod lower;
 mod lower_or_equal;
+use super::macros::bin_num_op::ACCEPTED_TYPE;
+
 pub use {
     equal::Equal, greater::Greater, greater_or_equal::GreaterOrEqual, lower::Lower,
     lower_or_equal::LowerOrEqual,
 };
 
 fn can_be_used(lhs: &Type, rhs: &Type) -> bool {
-    match (lhs, rhs) {
-        (Type::Int, Type::Int) | (Type::Float, Type::Float) => true,
-        (Type::Array(var_type), Type::Int) | (Type::Int, Type::Array(var_type))
-            if var_type.as_ref() == &Type::Int =>
-        {
-            true
-        }
-        (Type::Array(var_type), Type::Float) | (Type::Float, Type::Array(var_type))
-            if var_type.as_ref() == &Type::Float =>
-        {
-            true
-        }
-        _ => false,
-    }
+    Type::matches(&(lhs.clone(), rhs.clone()).into(), &ACCEPTED_TYPE)
 }
