67f6dc0a08f362a7936e24d1417026c2806a50bd
mpolosak <42770518+mpolosak@users.noreply.github.com>
2024-01-26 12:07:43 +0100

    Rewrite Attributes.parse

--- macros/src/attributes.rs
+++ macros/src/attributes.rs
@@ -18,22 +18,29 @@ impl Attributes {
         let mut new = Attributes::default();
         for MetaNameValue { path, value, .. } in attr {
             let path = quote!(#path).to_string();
-            if path == "name" {
-                new.name = match value {
+            match (path.as_ref(), value) {
+                (
+                    "name",
                     Expr::Lit(ExprLit {
                         lit: Lit::Str(lit), ..
-                    }) => Some(lit.value().into()),
-                    _ => panic!("{path} must be str literal"),
+                    }),
+                ) => {
+                    new.name = Some(lit.value().into());
                 }
-            } else if path == "return_type" {
-                new.return_type = match value {
+                (
+                    "return_type",
                     Expr::Lit(ExprLit {
                         lit: Lit::Str(lit), ..
-                    }) => Some(
+                    }),
+                ) => {
+                    new.return_type = Some(
                         quote!({use std::str::FromStr; simplesl::variable::Type::from_str(#lit).unwrap()}),
-                    ),
-                    _ => panic!("{path} must be bool"),
+                    );
                 }
+                ("name" | "return_type", _) => {
+                    panic!("{path} must be str literal");
+                }
+                _ => (),
             }
         }
         new
