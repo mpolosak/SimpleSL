5f1b7aeaa41a51033c8b8f1ee69f53763eeec08a
Michał Połosak <mpolosak@op.pl>
2024-03-13 21:34:01 +0100

    Refactor Instruction

--- src/instruction.rs
+++ src/instruction.rs
@@ -89,80 +89,111 @@ impl Instruction {
         local_variables: &LocalVariables,
     ) -> Result<Self, Error> {
         PRATT_PARSER
-            .map_primary(|pair| match pair.as_rule() {
-                Rule::expr => Self::new_expression(pair, interpreter, local_variables),
-                Rule::ident => {
-                    let ident = pair.as_str();
-                    local_variables.get(ident).map_or_else(
-                        || {
-                            interpreter
-                                .get_variable(ident)
-                                .cloned()
-                                .map(Instruction::from)
-                                .ok_or_else(|| Error::VariableDoesntExist(ident.into()))
-                        },
-                        |var| match var.clone() {
-                            LocalVariable::Variable(variable) => Ok(Self::Variable(variable)),
-                            local_variable => Ok(Self::LocalVariable(ident.into(), local_variable)),
-                        },
-                    )
-                }
-                Rule::int | Rule::float | Rule::string | Rule::void => {
-                    Variable::try_from(pair).map(Instruction::from)
-                }
-                Rule::tuple => Tuple::create_instruction(pair, interpreter, local_variables),
-                Rule::array => Array::create_instruction(pair, interpreter, local_variables),
-                Rule::array_repeat => {
-                    ArrayRepeat::create_instruction(pair, interpreter, local_variables)
-                }
-                Rule::function => {
-                    AnonymousFunction::create_instruction(pair, interpreter, local_variables)
-                }
-                rule => unexpected(rule),
+            .map_primary(|pair| Self::create_primary(pair, interpreter, local_variables))
+            .map_prefix(|op, rhs| Self::create_prefix(op, rhs?))
+            .map_infix(|lhs, op, rhs| {
+                Self::create_infix(op, lhs?, rhs?, local_variables, interpreter)
             })
-            .map_prefix(|op, rhs| match op.as_rule() {
-                Rule::not => Not::create_instruction(rhs?),
-                Rule::bitwise_not => BitwiseNot::create_instruction(rhs?),
-                Rule::unary_minus => UnaryMinus::create_instruction(rhs?),
-                rule => unexpected(rule),
-            })
-            .map_infix(|lhs, op, rhs| match op.as_rule() {
-                Rule::pow => Pow::create_op(lhs?, rhs?),
-                Rule::multiply => Multiply::create_op(lhs?, rhs?),
-                Rule::add => Add::create_op(lhs?, rhs?),
-                Rule::subtract => Subtract::create_op(lhs?, rhs?),
-                Rule::divide => Divide::create_op(lhs?, rhs?),
-                Rule::modulo => Modulo::create_op(lhs?, rhs?),
-                Rule::equal => Equal::create_op(lhs?, rhs?),
-                Rule::lower => Lower::create_op(lhs?, rhs?),
-                Rule::lower_equal => LowerOrEqual::create_op(lhs?, rhs?),
-                Rule::greater => Greater::create_op(lhs?, rhs?),
-                Rule::greater_equal => GreaterOrEqual::create_op(lhs?, rhs?),
-                Rule::map => Map::create_op(lhs?, rhs?),
-                Rule::filter => Filter::create_op(lhs?, rhs?),
-                Rule::bitwise_and => BitwiseAnd::create_op(lhs?, rhs?),
-                Rule::bitwise_or => BitwiseOr::create_op(lhs?, rhs?),
-                Rule::xor => Xor::create_op(lhs?, rhs?),
-                Rule::rshift => RShift::create_op(lhs?, rhs?),
-                Rule::lshift => LShift::create_op(lhs?, rhs?),
-                Rule::and => And::create_op(lhs?, rhs?),
-                Rule::or => Or::create_op(lhs?, rhs?),
-                Rule::reduce => {
-                    Reduce::create_instruction(lhs?, op, rhs?, local_variables, interpreter)
-                }
-                rule => unexpected(rule),
-            })
-            .map_postfix(|lhs, op| match op.as_rule() {
-                Rule::at => At::create_instruction(lhs?, op, interpreter, local_variables),
-                Rule::type_filter => {
-                    TypeFilter::create_instruction(lhs?, op.into_inner().next().unwrap())
-                }
-                Rule::function_call => {
-                    FunctionCall::create_instruction(lhs?, op, interpreter, local_variables)
-                }
-                rule => unexpected(rule),
-            })
+            .map_postfix(|lhs, op| Self::create_postfix(op, lhs?, interpreter, local_variables))
             .parse(pair.into_inner())
+    }
+
+    fn create_primary(
+        pair: Pair<'_, Rule>,
+        interpreter: &Interpreter<'_>,
+        local_variables: &LocalVariables<'_>,
+    ) -> Result<Instruction, Error> {
+        match pair.as_rule() {
+            Rule::expr => Self::new_expression(pair, interpreter, local_variables),
+            Rule::ident => {
+                let ident = pair.as_str();
+                local_variables.get(ident).map_or_else(
+                    || {
+                        interpreter
+                            .get_variable(ident)
+                            .cloned()
+                            .map(Instruction::from)
+                            .ok_or_else(|| Error::VariableDoesntExist(ident.into()))
+                    },
+                    |var| match var.clone() {
+                        LocalVariable::Variable(variable) => Ok(Self::Variable(variable)),
+                        local_variable => Ok(Self::LocalVariable(ident.into(), local_variable)),
+                    },
+                )
+            }
+            Rule::int | Rule::float | Rule::string | Rule::void => {
+                Variable::try_from(pair).map(Instruction::from)
+            }
+            Rule::tuple => Tuple::create_instruction(pair, interpreter, local_variables),
+            Rule::array => Array::create_instruction(pair, interpreter, local_variables),
+            Rule::array_repeat => {
+                ArrayRepeat::create_instruction(pair, interpreter, local_variables)
+            }
+            Rule::function => {
+                AnonymousFunction::create_instruction(pair, interpreter, local_variables)
+            }
+            rule => unexpected(rule),
+        }
+    }
+
+    fn create_prefix(op: Pair<'_, Rule>, rhs: Self) -> Result<Self, Error> {
+        match op.as_rule() {
+            Rule::not => Not::create_instruction(rhs),
+            Rule::bitwise_not => BitwiseNot::create_instruction(rhs),
+            Rule::unary_minus => UnaryMinus::create_instruction(rhs),
+            rule => unexpected(rule),
+        }
+    }
+
+    fn create_infix(
+        op: Pair<'_, Rule>,
+        lhs: Self,
+        rhs: Self,
+        local_variables: &LocalVariables<'_>,
+        interpreter: &Interpreter<'_>,
+    ) -> Result<Self, Error> {
+        match op.as_rule() {
+            Rule::pow => Pow::create_op(lhs, rhs),
+            Rule::multiply => Multiply::create_op(lhs, rhs),
+            Rule::add => Add::create_op(lhs, rhs),
+            Rule::subtract => Subtract::create_op(lhs, rhs),
+            Rule::divide => Divide::create_op(lhs, rhs),
+            Rule::modulo => Modulo::create_op(lhs, rhs),
+            Rule::equal => Equal::create_op(lhs, rhs),
+            Rule::lower => Lower::create_op(lhs, rhs),
+            Rule::lower_equal => LowerOrEqual::create_op(lhs, rhs),
+            Rule::greater => Greater::create_op(lhs, rhs),
+            Rule::greater_equal => GreaterOrEqual::create_op(lhs, rhs),
+            Rule::map => Map::create_op(lhs, rhs),
+            Rule::filter => Filter::create_op(lhs, rhs),
+            Rule::bitwise_and => BitwiseAnd::create_op(lhs, rhs),
+            Rule::bitwise_or => BitwiseOr::create_op(lhs, rhs),
+            Rule::xor => Xor::create_op(lhs, rhs),
+            Rule::rshift => RShift::create_op(lhs, rhs),
+            Rule::lshift => LShift::create_op(lhs, rhs),
+            Rule::and => And::create_op(lhs, rhs),
+            Rule::or => Or::create_op(lhs, rhs),
+            Rule::reduce => Reduce::create_instruction(lhs, op, rhs, local_variables, interpreter),
+            rule => unexpected(rule),
+        }
+    }
+
+    fn create_postfix(
+        op: Pair<'_, Rule>,
+        lhs: Self,
+        interpreter: &Interpreter<'_>,
+        local_variables: &LocalVariables<'_>,
+    ) -> Result<Self, Error> {
+        match op.as_rule() {
+            Rule::at => At::create_instruction(lhs, op, interpreter, local_variables),
+            Rule::type_filter => {
+                TypeFilter::create_instruction(lhs, op.into_inner().next().unwrap())
+            }
+            Rule::function_call => {
+                FunctionCall::create_instruction(lhs, op, interpreter, local_variables)
+            }
+            rule => unexpected(rule),
+        }
     }
 }
 
@@ -170,10 +201,10 @@ impl Exec for Instruction {
     fn exec(&self, interpreter: &mut Interpreter) -> ExecResult {
         match self {
             Self::Variable(var) => Ok(var.clone()),
-            Self::LocalVariable(ident, _) => Ok(interpreter
+            Self::LocalVariable(ident, _) => interpreter
                 .get_variable(ident)
                 .cloned()
-                .unwrap_or_else(|| panic!("Tried to get variable {ident} that doest exist"))),
+                .ok_or_else(|| panic!("Tried to get variable {ident} that doest exist")),
             Self::AnonymousFunction(function) => function.exec(interpreter),
             Self::Tuple(function) => function.exec(interpreter),
             Self::Other(other) => other.exec(interpreter),
