328806a0764c36752630ec84e7ebbbb2c13a18a9
Michał Połosak <mpolosak@op.pl>
2024-03-13 21:10:48 +0100

    Merge set_if and set_if_else rules

--- src/instruction.rs
+++ src/instruction.rs
@@ -73,9 +73,7 @@ impl Instruction {
             Rule::block => Block::create_instruction(pair, interpreter, local_variables),
             Rule::import => Import::create_instruction(pair, interpreter, local_variables),
             Rule::if_else => IfElse::create_instruction(pair, interpreter, local_variables),
-            Rule::set_if_else | Rule::set_if => {
-                SetIfElse::create_instruction(pair, interpreter, local_variables)
-            }
+            Rule::set_if_else => SetIfElse::create_instruction(pair, interpreter, local_variables),
             Rule::r#match => Match::create_instruction(pair, interpreter, local_variables),
             Rule::function_declaration => {
                 FunctionDeclaration::create_instruction(pair, interpreter, local_variables)
