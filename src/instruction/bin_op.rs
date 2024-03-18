d51cc12368661aee643744236a0c388c4c2bd881
Michał Połosak <mpolosak@op.pl>
2024-03-18 13:06:02 +0100

    Move Instruction::create_infix to bin_op.rs

--- src/instruction/bin_op.rs
+++ src/instruction/bin_op.rs
@@ -6,15 +6,18 @@ mod math;
 mod ord;
 mod reduce;
 mod shift;
+use super::local_variable::LocalVariables;
 use crate::{
     instruction::{
         traits::{CanBeUsed, ToResult},
         Instruction,
     },
+    parse::{unexpected, Rule},
     variable::{ReturnType, Variable},
-    Error,
+    Error, Interpreter,
 };
 use duplicate::duplicate_item;
+use pest::iterators::Pair;
 pub use reduce::*;
 
 #[duplicate_item(T; [BitwiseAnd]; [BitwiseOr]; [Xor]; [Equal]; [Filter]; [Map]; [And]; [Or];
@@ -64,5 +67,40 @@ impl T {
 impl Equal {
     pub fn exec(lhs: Variable, rhs: Variable) -> Variable {
         (lhs == rhs).into()
+    }
+}
+
+impl Instruction {
+    pub fn create_infix(
+        op: Pair<'_, Rule>,
+        lhs: Self,
+        rhs: Self,
+        local_variables: &LocalVariables<'_>,
+        interpreter: &Interpreter<'_>,
+    ) -> Result<Self, Error> {
+        match op.as_rule() {
+            Rule::pow => Pow::create_op(lhs, rhs),
+            Rule::multiply => Multiply::create_op(lhs, rhs),
+            Rule::add => Add::create_op(lhs, rhs),
+            Rule::subtract => Subtract::create_op(lhs, rhs),
+            Rule::divide => Divide::create_op(lhs, rhs),
+            Rule::modulo => Modulo::create_op(lhs, rhs),
+            Rule::equal => Equal::create_op(lhs, rhs),
+            Rule::lower => Lower::create_op(lhs, rhs),
+            Rule::lower_equal => LowerOrEqual::create_op(lhs, rhs),
+            Rule::greater => Greater::create_op(lhs, rhs),
+            Rule::greater_equal => GreaterOrEqual::create_op(lhs, rhs),
+            Rule::map => Map::create_op(lhs, rhs),
+            Rule::filter => Filter::create_op(lhs, rhs),
+            Rule::bitwise_and => BitwiseAnd::create_op(lhs, rhs),
+            Rule::bitwise_or => BitwiseOr::create_op(lhs, rhs),
+            Rule::xor => Xor::create_op(lhs, rhs),
+            Rule::rshift => RShift::create_op(lhs, rhs),
+            Rule::lshift => LShift::create_op(lhs, rhs),
+            Rule::and => And::create_op(lhs, rhs),
+            Rule::or => Or::create_op(lhs, rhs),
+            Rule::reduce => Reduce::create_instruction(lhs, op, rhs, local_variables, interpreter),
+            rule => unexpected(rule),
+        }
     }
 }
