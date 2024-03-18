7c6f66d69be272cbc0c57fe191943d627c4303c5
Michał Połosak <mpolosak@op.pl>
2024-03-18 10:29:56 +0100

    Merge multiply.rs and subtract.rs into math.rs

--- src/instruction/bin_op/math.rs
+++ src/instruction/bin_op/math.rs
@@ -1,12 +1,50 @@
 mod add;
 mod divide;
-mod multiply;
 mod pow;
-mod subtract;
+use super::binOp;
+use crate::{
+    instruction::Instruction,
+    variable::{Type, Typed, Variable},
+};
+use duplicate::duplicate_item;
 pub use {
     add::Add,
     divide::{Divide, Modulo},
-    multiply::Multiply,
     pow::Pow,
-    subtract::Subtract,
 };
+
+binOp!(Multiply, "*");
+binOp!(Subtract, "-");
+
+#[duplicate_item(T op symbol; [Multiply] [lhs*rhs] [*]; [Subtract] [lhs-rhs] [-])]
+impl T {
+    pub fn create_from_instructions(lhs: Instruction, rhs: Instruction) -> Instruction {
+        match (lhs, rhs) {
+            (Instruction::Variable(lhs), Instruction::Variable(rhs)) => Self::exec(lhs, rhs).into(),
+            (lhs, rhs) => Self { lhs, rhs }.into(),
+        }
+    }
+
+    pub fn exec(lhs: Variable, rhs: Variable) -> Variable {
+        match (lhs, rhs) {
+            (Variable::Int(lhs), Variable::Int(rhs)) => (op).into(),
+            (Variable::Float(lhs), Variable::Float(rhs)) => (op).into(),
+            (array @ Variable::Array(_), _) | (_, array @ Variable::Array(_))
+                if array.as_type() == Type::EmptyArray =>
+            {
+                array
+            }
+            (value, Variable::Array(array)) => array
+                .iter()
+                .cloned()
+                .map(|element| Self::exec(value.clone(), element))
+                .collect(),
+            (Variable::Array(array), value) => array
+                .iter()
+                .cloned()
+                .map(|element| Self::exec(element, value.clone()))
+                .collect(),
+            (lhs, rhs) => panic!("Tried to calc {lhs} {} {rhs}", stringify!(symbol)),
+        }
+    }
+}
