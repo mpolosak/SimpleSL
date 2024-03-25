dac5508d3c32cea8477fefa2ee6bef34c269788b
Michał Połosak <mpolosak@op.pl>
2024-03-25 13:35:10 +0100

    impl From<Pair<'_, Rule>> for FunctionType dont use for loop

--- src/variable/function_type.rs
+++ src/variable/function_type.rs
@@ -60,15 +60,9 @@ impl<T: Into<Type>> BitOr<T> for FunctionType {
 #[doc(hidden)]
 impl From<Pair<'_, Rule>> for FunctionType {
     fn from(pair: Pair<'_, Rule>) -> Self {
-        let mut return_type = Type::Any;
-        let mut params: Box<[Type]> = [].into();
-        for pair in pair.into_inner() {
-            if pair.as_rule() == Rule::function_type_params {
-                params = pair.into_inner().map(Type::from).collect();
-            } else {
-                return_type = Type::from(pair);
-            }
-        }
+        let mut pairs = pair.into_inner();
+        let params: Box<[Type]> = pairs.next().unwrap().into_inner().map(Type::from).collect();
+        let return_type = Type::from(pairs.next().unwrap());
         Self {
             params,
             return_type,
