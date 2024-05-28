42c2ca3e1772a14182f0dacfe762f5bcddfd6280
Michał Połosak <mpolosak@op.pl>
2024-05-28 20:06:27 +0200

    Add var_type! macro

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,14 +1,18 @@
 #![warn(clippy::pedantic)]
-use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, ItemFn};
 mod attributes;
 mod utils;
+mod var_type;
 use attributes::Attributes;
+use pest::Parser;
+use proc_macro::TokenStream;
+use quote::quote;
+use simplesl_parser::{Rule, SimpleSLParser};
+use syn::{parse_macro_input, ItemFn};
 use utils::{
     args_from_function_params, args_import_from_function_params, function_params_from_itemfn,
     get_body, get_return_type, params_from_function_params,
 };
+use var_type::type_token_from_pair;
 
 #[proc_macro_attribute]
 pub fn export_function(attr: TokenStream, function: TokenStream) -> TokenStream {
@@ -40,4 +44,14 @@ pub fn export_function(attr: TokenStream, function: TokenStream) -> TokenStream 
         }
     )
     .into()
+}
+
+#[proc_macro]
+pub fn var_type(item: TokenStream) -> TokenStream {
+    let item_str = item.to_string();
+    let pair = SimpleSLParser::parse(Rule::r#type, &item_str)
+        .unwrap_or_else(|error| panic!("{error}"))
+        .next()
+        .unwrap();
+    type_token_from_pair(pair).into()
 }
---
+++ macros/src/var_type.rs
@@ -0,0 +1,46 @@
+use pest::iterators::Pair;
+use quote::quote;
+use simplesl_parser::{unexpected, Rule};
+
+pub fn type_token_from_pair(pair: Pair<Rule>) -> quote::__private::TokenStream {
+    match pair.as_rule() {
+        Rule::int_type => quote!(simplesl::variable::Type::Int),
+        Rule::float_type => quote!(simplesl::variable::Type::Float),
+        Rule::string_type => quote!(simplesl::variable::Type::String),
+        Rule::void => quote!(simplesl::variable::Type::Void),
+        Rule::any => quote!(simplesl::variable::Type::Any),
+        Rule::multi => pair
+            .into_inner()
+            .map(|pair| type_token_from_pair(pair))
+            .reduce(|acc, curr| quote!(#curr | # acc))
+            .unwrap(),
+        Rule::array_type => {
+            let element_type = pair.into_inner().next().map(type_token_from_pair).unwrap();
+            quote!(simplesl::variable::Type::Array(#element_type.into()))
+        }
+        Rule::tuple_type => {
+            let elements = pair
+                .into_inner()
+                .map(type_token_from_pair)
+                .reduce(|acc, curr| quote!(#curr, # acc));
+            quote!(simplesl::variable::Type::Tuple([#elements].into()))
+        }
+        Rule::function_type => {
+            let mut pairs = pair.into_inner();
+            let params = pairs
+                .next()
+                .unwrap()
+                .into_inner()
+                .map(type_token_from_pair)
+                .reduce(|acc, curr| quote!(#curr, # acc));
+            let return_type = pairs.next().map(type_token_from_pair).unwrap();
+            quote!(simplesl::variable::Type::Function(
+                simplesl::variable::FunctionType {
+                    params: [#params].into(),
+                    return_type: #return_type
+                }.into()
+            ))
+        }
+        rule => unexpected(rule),
+    }
+}
