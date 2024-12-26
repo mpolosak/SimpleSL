91f2d730c8ad7888fc822135c1e54b0cec3efb96
Michał Połosak <mpolosak@op.pl>
2024-12-26 22:03:05 +0100

    Add test for $+ operator

--- src/instruction/reduce/sum.rs
+++ src/instruction/reduce/sum.rs
@@ -45,3 +45,89 @@ pub fn exec(var: Variable) -> ExecResult {
         Ok(STRING_SUM.as_function().unwrap().exec_with_args(&[var])?)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate as simplesl;
+    use crate::{
+        instruction::reduce::sum::ACCEPTED_TYPE, unary_operator::UnaryOperator, variable::Variable,
+        Code, Error, Interpreter,
+    };
+    use simplesl_macros::{var, var_type};
+    const OP: UnaryOperator = UnaryOperator::Sum;
+
+    #[test]
+    fn sum() {
+        assert_eq!(
+            parse_and_exec("[45, 76, 15]$+"),
+            Err(Error::IncorectUnaryOperatorOperand {
+                ins: "[45, 76, 15]".into(),
+                op: OP,
+                expected: ACCEPTED_TYPE.clone(),
+                given: var_type!([int])
+            })
+        );
+        assert_eq!(
+            parse_and_exec(r#""abc"$+"#),
+            Err(Error::IncorectUnaryOperatorOperand {
+                ins: r#""abc""#.into(),
+                op: OP,
+                expected: ACCEPTED_TYPE.clone(),
+                given: var_type!(string)
+            })
+        );
+        assert_eq!(
+            parse_and_exec("x:= () -> int {return 5;} x$+"),
+            Err(Error::IncorectUnaryOperatorOperand {
+                ins: "x".into(),
+                op: OP,
+                expected: ACCEPTED_TYPE.clone(),
+                given: var_type!(() -> int)
+            })
+        );
+        assert_eq!(
+            parse_and_exec("x:= (a: int) -> (bool, int) {return (true, a);} x$+"),
+            Err(Error::IncorectUnaryOperatorOperand {
+                ins: "x".into(),
+                op: OP,
+                expected: ACCEPTED_TYPE.clone(),
+                given: var_type!((int) -> (bool, int))
+            })
+        );
+        assert_eq!(parse_and_exec("[45, 16, 3]~$+"), Ok(var!(64)));
+        assert_eq!(parse_and_exec("[5.5, 6.5, 7.4]~$+"), Ok(var!(19.4)));
+        assert_eq!(parse_and_exec(r#"["a", "6.5", "$"]~$+"#), Ok(var!("a6.5$")));
+        assert_eq!(
+            parse_and_exec("[45, 16.5, 3]~$+"),
+            Err(Error::IncorectUnaryOperatorOperand {
+                ins: "[45, 16.5, 3]".into(),
+                op: OP,
+                expected: ACCEPTED_TYPE.clone(),
+                given: var_type!(() -> (bool, int|float))
+            })
+        );
+        assert_eq!(
+            parse_and_exec(
+                "x:=() -> (()->(bool, int)|() -> (bool, float)){
+                    return [45, 16, 45]~;
+                }
+                x$+"
+            ),
+            Ok(var!(106))
+        );
+        assert_eq!(
+            parse_and_exec(
+                "x:=() -> (()->(bool, int)|() -> (bool, float)){
+                    return [4.5, 1.6, 4.5]~;
+                }
+                x$+"
+            ),
+            Ok(var!(10.6))
+        );
+    }
+
+    fn parse_and_exec(script: &str) -> Result<Variable, Error> {
+        Code::parse(&Interpreter::without_stdlib(), script)
+            .and_then(|code| code.exec().map_err(Error::from))
+    }
+}
